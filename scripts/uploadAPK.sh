1dcd6eb0b975775750c17d950c391b6296ded0ff
Clark Ewerton <clarkewerton@hotmail.com>
2025-04-21 21:53:31 -0300

    Update uploadAPK.sh

--- scripts/uploadAPK.sh
+++ scripts/uploadAPK.sh
@@ -2,11 +2,11 @@
       adb -s emulator-5554 install -t -g ./app/googleCalculator.apk
       
       echo "Instalando APK no emulador 5556..."
-      #adb -s emulator-5556 install -t -g ./app/googleCalculator.apk
+      adb -s emulator-5556 install -t -g ./app/googleCalculator.apk
 
       # 3. Verificação da instalação
       echo "Verificando pacotes no emulador 5554:"
       adb -s emulator-5554 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5554"
       
       #echo "Verificando pacotes no emulador 5556:"
-      #adb -s emulator-5556 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5556
+      adb -s emulator-5556 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5556
